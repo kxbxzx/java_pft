f4e119edd38d53b5fce101e9fd02aaba4aaca823
Alexander Stepanov <xkbzzx@gmail.com>
2017-04-02 21:46:51 +0300

    upd

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -10,11 +11,13 @@ public class ContactDeletionTest extends TestBase {
 
     @Test
     public void testContactDeletion(){
+
         if (! app.getContactHelper().isThereAContact()){
             app.getContactHelper().createContact(new ContactData("Alexander", "Stepanov", "xkbzzx@gmail.com", "Moscow", "test1"), true);
         }
         app.getContactHelper().initContactModification();
         app.getContactHelper().deleteContact();
+
     }
 
 }
