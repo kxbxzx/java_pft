88aef288044c991e7fedc84b7565aa36adb5677f
Alexander Stepanov <xkbzzx@gmail.com>
2017-04-15 08:19:57 +0300

    upd

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,10 +1,10 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
-    private  int id;
-    private final String name;
-    private final String header;
-    private final String footer;
+    private int id = Integer.MAX_VALUE;
+    private String name;
+    private String header;
+    private String footer;
 
     public int getId() {
         return id;
@@ -23,21 +23,6 @@ public class GroupData {
     @Override
     public int hashCode() {
         return name != null ? name.hashCode() : 0;
-    }
-
-    public GroupData(String name, String header, String footer) {
-        this.id = Integer.MAX_VALUE;
-        this.name = name;
-
-        this.header = header;
-        this.footer = footer;
-    }
-
-    public GroupData(int id, String name, String header, String footer) {
-        this.id = id;
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
     }
 
     public String getName() {
@@ -60,8 +45,24 @@ public class GroupData {
                 '}';
     }
 
-    public void setId(int id) {
+    public GroupData withId(int id) {
         this.id = id;
+        return this;
+    }
+
+    public GroupData withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public GroupData withHeader(String header) {
+        this.header = header;
+        return this;
+    }
+
+    public GroupData withFooter(String footer) {
+        this.footer = footer;
+        return this;
     }
 
 }
