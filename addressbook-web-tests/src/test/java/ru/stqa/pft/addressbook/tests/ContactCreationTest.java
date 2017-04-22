308e2988d2dd5453e21ec74c7ceeb1d8a21b7fab
Alexander Stepanov <xkbzzx@gmail.com>
2017-04-22 12:40:50 +0300

    ContactCreationTest - JSON

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,12 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.hamcrest.CoreMatchers;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
@@ -14,17 +22,32 @@ import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTest extends TestBase {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line != null){
+            json +=line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validContactsFromJson")
+    public void testContactCreation(ContactData contact) {
         app.contact().gotoHome();
         Contacts before = app.contact().all();
-        File photo = new File("src/test/resources/java_logo.png");
-        ContactData contact = new ContactData()
-                .withName("Alexander")
-                .withSurname("Stepanov")
-                .withAddress("Moscow")
-                .withEmail("xkbzzx@gmail.com")
-                .withPhoto(photo);
+        // File photo = new File("src/test/resources/java_logo.png");
+        // ContactData contact = new ContactData()
+        //        .withName("Alexander")
+        //        .withSurname("Stepanov")
+        //        .withAddress("Moscow")
+        //        .withEmail("xkbzzx@gmail.com")
+        //        .withPhoto(photo);
         app.contact().create(contact);
         Contacts after = app.contact().all();
         assertThat(after.size(), equalTo(before.size() + 1));
